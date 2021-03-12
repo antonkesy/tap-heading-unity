3dca8db89496ff3140a7ad07ab1dd4501a61a89a
Anton Kesy <anton@kesy.de>
2021-03-12 20:39:33 +0100

    UI Shadow

--- New Unity Project/Assets/Scripts/UIManager.cs
+++ New Unity Project/Assets/Scripts/UIManager.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI scoreTextShadow;
 
     internal void UpdateScoreText(int newScore)
     {
         scoreText.text = newScore.ToString();
+        scoreTextShadow.text = newScore.ToString();
     }
 }
