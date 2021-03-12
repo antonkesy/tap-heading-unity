77feeabe0e5681973c1155141de38d7f5d3759cc
Anton Kesy <anton@kesy.de>
2021-03-12 23:21:17 +0100

    TODOs

--- TapHeadingAndroid/Assets/Scripts/GameManager.cs
+++ TapHeadingAndroid/Assets/Scripts/GameManager.cs
@@ -24,6 +24,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        //TODO("to touch")
         if (Input.GetMouseButtonDown(0))
             if (_isRunning)
             {
