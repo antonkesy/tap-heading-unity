79d26b4319bb6fbeb81fdc96e0d194aa9b0a24f1
Anton Kesy <anton@kesy.de>
2021-03-15 02:27:10 +0100

    Optimized UIManager
    yeah ... typo in the last commits :)

--- TapHeadingAndroid/Assets/Scripts/UIManager.cs
+++ TapHeadingAndroid/Assets/Scripts/UIManager.cs
@@ -26,33 +26,26 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private GameManager gameManager;
+    [Header("Manager")] [SerializeField] private GameManager gameManager;
+    [SerializeField] private UIMenuManager menuManager;
+    [SerializeField] private AudioManager audioManager;
     [Header("Play")] [SerializeField] private TextMeshProUGUI scoreText;
     private UIFader _scoreTextFader;
     [SerializeField] private TextMeshProUGUI scoreTextShadow;
     private UIFader _scoreTextShadowFader;
-    [Header("Menu")] [SerializeField] private GameObject menu;
-    [SerializeField] private UIMenuManager menuManager;
-    [SerializeField] private TextMeshProUGUI highScoreText;
-    [SerializeField] private TextMeshProUGUI gameTitleText;
+    [Header("Menu")] [SerializeField] private TextMeshProUGUI highScoreText;
     [SerializeField] private TextMeshProUGUI tapToStartText;
-
     [SerializeField] private GameObject aboutPanel;
 
     private bool _isPlaying;
     private bool _isSoundOn;
 
 
-    [SerializeField] private AudioManager audioManager;
-
     private void Start()
     {
         _scoreTextFader = scoreText.GetComponent<UIFader>();
         _scoreTextShadowFader = scoreTextShadow.GetComponent<UIFader>();
-
-
         _isSoundOn = PlayerPrefs.GetInt("soundOff", 1) == 1;
-
         UpdateHighScoreText(PlayerPrefs.GetInt("highScore", 0));
     }
 
@@ -64,7 +57,7 @@ public class UIManager : MonoBehaviour
 
     internal void UpdateHighScoreText(int newScore)
     {
-        highScoreText.text = "BEST: " + newScore;
+        highScoreText.text = $"BEST: {newScore}";
     }
 
     internal void ShowPlayUI()
@@ -79,26 +72,25 @@ public class UIManager : MonoBehaviour
 
     internal void ShowReturningMenuUI()
     {
+        ShowMenu("TAP TO RESTART");
+        menuManager.FadeInMenu();
+    }
+
+    private void ShowMenu(string tapToText)
+    {
         _isPlaying = false;
         aboutPanel.SetActive(false);
-        //Menu
         menuManager.SetSound(_isSoundOn);
-        menuManager.FadeInMenu();
-        tapToStartText.text = "TAP TO RESTART";
+        tapToStartText.text = tapToText;
     }
 
     internal void ShowStartMenuUI()
     {
-        _isPlaying = false;
-        aboutPanel.SetActive(false);
-        //Play
         _scoreTextFader.Fade(false, 0);
         _scoreTextShadowFader.Fade(false, 0);
-        //Menu
-        menuManager.SetSound(_isSoundOn);
+        ShowMenu("TAP TO START");
         menuManager.FadeInStart();
         StartCoroutine(WaitForStartCallback());
-        tapToStartText.text = "TAP TO START";
         menuManager.SlideInGameTitle();
     }
 
@@ -127,14 +119,9 @@ public class UIManager : MonoBehaviour
         {
             PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptAlways, result =>
             {
-                switch (result)
-                {
-                    case SignInStatus.Success:
-                        PlayGamesPlatform.Instance.ShowLeaderboardUI();
-                        return;
-                    default:
-                        return;
-                }
+                if (result == SignInStatus.Success)
+
+                    PlayGamesPlatform.Instance.ShowLeaderboardUI();
             });
         }
         else
@@ -151,14 +138,8 @@ public class UIManager : MonoBehaviour
         {
             PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptAlways, result =>
             {
-                switch (result)
-                {
-                    case SignInStatus.Success:
-                        PlayGamesPlatform.Instance.ShowAchievementsUI();
-                        return;
-                    default:
-                        return;
-                }
+                if (result == SignInStatus.Success)
+                    PlayGamesPlatform.Instance.ShowAchievementsUI();
             });
         }
         else
@@ -169,50 +150,49 @@ public class UIManager : MonoBehaviour
 
     public void OnSoundOnButtonClick()
     {
-        if (_isPlaying) return;
-        audioManager.SetSound(true);
-        audioManager.PlayTapUI();
-        PlayerPrefs.SetInt("soundOff", 0);
-        _isSoundOn = false;
-        menuManager.SetSound(false);
+        OnSoundButtonClick(true);
     }
 
     public void OnSoundOffButtonClick()
     {
+        OnSoundButtonClick(false);
+    }
+
+    private void OnSoundButtonClick(bool soundOn)
+    {
         if (_isPlaying) return;
-        audioManager.SetSound(false);
+        audioManager.SetSound(soundOn);
         audioManager.PlayTapUI();
-        PlayerPrefs.SetInt("soundOff", 1);
-        menuManager.SetSound(true);
-        _isSoundOn = true;
+        PlayerPrefs.SetInt("soundOff", soundOn ? 0 : 1);
+        _isSoundOn = !soundOn;
+        menuManager.SetSound(!soundOn);
+    }
+
+    private void OpenWebsite(string url)
+    {
+        if (_isPlaying) return;
+        audioManager.PlayTapUI();
+        Application.OpenURL(url);
     }
 
     public void OnWebsiteButtonClick()
     {
-        if (_isPlaying) return;
-        audioManager.PlayTapUI();
-        Application.OpenURL("https://poorskill.com/");
+        OpenWebsite("https://poorskill.com/");
     }
 
     public void OnGitHubButtonClick()
     {
-        if (_isPlaying) return;
-        audioManager.PlayTapUI();
-        Application.OpenURL("https://github.com/PoorSkill/tap-heading-unity");
+        OpenWebsite("https://github.com/PoorSkill/tap-heading-unity");
     }
 
     public void OnYouTubeButtonClick()
     {
-        if (_isPlaying) return;
-        audioManager.PlayTapUI();
-        Application.OpenURL("https://www.youtube.com/channel/UCgMifJ1aQnFFkwGgrxHSPjg");
+        OpenWebsite("https://www.youtube.com/channel/UCgMifJ1aQnFFkwGgrxHSPjg");
     }
 
     public void OnPlayStoreButtonClick()
     {
-        if (_isPlaying) return;
-        audioManager.PlayTapUI();
-        Application.OpenURL("https://play.google.com/store/apps/details?id=com.poorskill.tapheading");
+        OpenWebsite("https://play.google.com/store/apps/details?id=com.poorskill.tapheading");
     }
 
     internal bool isAboutOn()
