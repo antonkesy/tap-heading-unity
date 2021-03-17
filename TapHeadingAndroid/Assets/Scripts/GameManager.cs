5935f7fd2fcbf529788091477a4f0f33451810cf
Anton Kesy <anton@kesy.de>
2021-03-17 09:21:10 +0100

    Comment out DebugInput

--- TapHeadingAndroid/Assets/Scripts/GameManager.cs
+++ TapHeadingAndroid/Assets/Scripts/GameManager.cs
@@ -69,7 +69,7 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         //DEBUG----------------------------------------------
-        ProcessEditorInput();
+        //ProcessEditorInput();
         //-------------------------------------------------
         ProcessUserInput();
     }
