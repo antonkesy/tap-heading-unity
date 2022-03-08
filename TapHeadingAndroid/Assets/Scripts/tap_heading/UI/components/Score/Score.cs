fcbc4be72ce13e629b66d479ad36a08ad40ed7f0
Anton Kesy <anton@kesy.de>
2022-03-08 17:06:30 +0100

    zoom in score

--- TapHeadingAndroid/Assets/Scripts/tap_heading/UI/components/Score/Score.cs
+++ TapHeadingAndroid/Assets/Scripts/tap_heading/UI/components/Score/Score.cs
@@ -1,5 +1,7 @@
 using tap_heading.UI.components.Text;
 using tap_heading.UI.utility.Transition;
+using tap_heading.UI.utility.Transition.Zoom;
+using TMPro;
 using UnityEngine;
 
 namespace tap_heading.UI.components.Score
@@ -8,20 +10,22 @@ namespace tap_heading.UI.components.Score
     {
         [SerializeField] private ShadowText playing;
         [SerializeField] private ShadowText menu;
+        [SerializeField] private ZoomUI playingZoom;
+        [SerializeField] private ZoomUI menuZoom;
 
         private ITransition _active;
 
         public void ShowPlaying()
         {
             _active?.Out();
-            _active = playing;
+            _active = playingZoom;
             _active.In();
         }
 
         public void ShowMenu()
         {
             _active?.Out();
-            _active = menu;
+            _active = menuZoom;
             _active.In();
         }
 
