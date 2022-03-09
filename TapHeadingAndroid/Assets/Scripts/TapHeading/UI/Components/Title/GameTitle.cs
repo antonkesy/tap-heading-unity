edd56238cdc49fdec7f9f780715948b6d5aedd1c
Anton Kesy <anton@kesy.de>
2022-03-09 21:33:26 +0100

    remove unused text

--- TapHeadingAndroid/Assets/Scripts/TapHeading/UI/Components/Title/GameTitle.cs
+++ TapHeadingAndroid/Assets/Scripts/TapHeading/UI/Components/Title/GameTitle.cs
@@ -1,4 +1,3 @@
-using TapHeading.UI.Components.Text;
 using TapHeading.UI.Utility.Transition;
 using TapHeading.UI.Utility.Transition.Slide;
 using UnityEngine;
@@ -7,12 +6,10 @@ namespace TapHeading.UI.Components.Title
 {
     public class GameTitle : MonoBehaviour, ITransition
     {
-        private ShadowText _text;
         private Slider _slider;
 
         private void Awake()
         {
-            _text = GetComponentInChildren<ShadowText>();
             _slider = GetComponentInChildren<Slider>();
         }
 
